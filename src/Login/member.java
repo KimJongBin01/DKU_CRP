8fc8a739dcd3387dea406b50bf3c5a3c3df044a6
KimJongBin01 <91369344+KimJongBin01@users.noreply.github.com>
2021-11-28 18:10:21 +0900

    Add files via upload

---
+++ src/Login/member.java
@@ -0,0 +1,31 @@
+package Login;
+
+public class member {
+    private String Id;
+    private String name;
+    private String password;
+    private String grade;
+
+    public member(String name, String grade, String Id, String password){
+        this.name = name;
+        this.grade = grade;
+        this.Id = Id;
+        this.password = password;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getId(){
+        return this.Id;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public String getGrade(){
+        return this.grade;
+    }
+}
